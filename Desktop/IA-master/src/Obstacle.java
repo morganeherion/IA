65082a83ba6a446caf7dc498e2a636bd8b9a2fbf
remimoncorge <remim97@hotmail.fr>
2018-10-15 15:06:05 +0200

    test

---
+++ Desktop/IA-master/src/Obstacle.java
@@ -0,0 +1,52 @@
+
+public class Obstacle {
+	
+	private boolean attrape;
+	private boolean depose;
+	private boolean present;
+
+	public Obstacle() {
+		attrape = false;
+		depose = false;
+		present = false;
+		}
+	
+	public void isPresent(float distanceInitiale, CapteurDistance distance, CapteurToucher toucher, Roues r, Pinces p) {
+		float d = 0;
+		d = distanceInitiale;
+		distance.enable();
+		while(d<=distanceInitiale) {
+			r.avance(40,2000);
+			d = distance.getDistance();
+		}
+		
+		r.quartTourDroite();
+		if(distance.getDistance()<distanceInitiale) {
+			this.isAttrape(toucher, r, p);
+		}
+		else  {
+			r.demiTourGauche();
+			if(distance.getDistance()<distanceInitiale) {
+				this.isAttrape(toucher, r, p);	
+			}
+			else {
+				r.quartTourDroite();
+				this.isAttrape(toucher, r, p);
+			}
+		}
+		distance.disable();
+	}
+		
+	public void isAttrape(CapteurToucher toucher, Roues r, Pinces p) {
+		while (!toucher.isTouche()) {
+			r.avance(40, 2000);
+		}
+		p.capturerPalet();
+		r.stop(); 
+		
+	}
+	
+	
+	
+
+}
---
+++ Desktop/IA-master/src/Roues.java
@@ -0,0 +1,147 @@
+import lejos.hardware.motor.UnregulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.utility.Delay;
+
+public class Roues {
+	
+	private int puissance;
+	private int sens; // restrict to 1 and 0
+	private boolean active;
+	UnregulatedMotor roueG = new UnregulatedMotor(MotorPort.A);
+	UnregulatedMotor roueD = new UnregulatedMotor(MotorPort.C);
+	
+	public Roues() {
+//		setPuissance(0);
+//		setSens(1);
+//		setActive(false);
+	}
+	
+	public int getPuissance() {
+		return puissance;
+	}
+	
+	public void setPuissance(int puissance) {
+		this.puissance = puissance;
+	}
+	
+	public int getSens() {
+		return sens;
+	}
+	
+	public void setSens(int sens) {
+		this.sens = sens;
+	}
+	
+	public boolean isActive() {
+		return active;
+	}
+	
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	
+	//indiquer la dur�e pour chaque m�thode de mouvement
+	//prendre en compte les donn�es des capteurs pour agir en fonction
+	
+	public void avance(int puissance) {
+		//tant qu'on dit d'avancer
+		roueG.setPower(puissance);
+		roueD.setPower(puissance);
+		
+		roueG.forward();
+		roueD.forward();
+		}
+	
+	public void avance(int puissance, int duree) {
+		//tant qu'on dit d'avancer
+		roueG.setPower(puissance);
+		roueD.setPower(puissance);
+		
+		roueG.forward();
+		roueD.forward();
+		
+		Delay.msDelay(duree);
+		}
+	
+	public void recule(int puissance) {
+		//tant qu'on dit de reculer
+		roueG.setPower(puissance);
+		roueD.setPower(puissance);
+		
+		roueG.backward();
+		roueD.backward();
+		}		
+	
+	//peut-etre juste faire genre une m�thode qui fait tourner
+	public void tourne(int p1, int p2, boolean tourneGauche) {
+		int pFaible;
+		int pHaute;
+		
+		if (p2 < p1) {
+			pFaible = p2;
+			pHaute = p1;
+		} else {
+			pFaible = p1;
+			pHaute = p2;
+		}
+		
+		if (tourneGauche) {
+			roueG.setPower(pHaute);
+			roueD.setPower(pFaible);
+		} else {
+			//tourneDroite du coup hein
+			roueG.setPower(pFaible);
+			roueD.setPower(pHaute);
+		}
+		
+		roueG.forward();
+		roueD.forward();
+		//� changer (valeurs "trouv�es" � t�tons)
+		Delay.msDelay(5000);
+		stop();
+		
+	}
+	
+	//on verra plus tard pour la direction
+	public void demiTour() {
+		roueG.setPower(80);
+		roueD.setPower(80);
+		
+		roueG.forward();
+		roueD.backward();
+		//� changer (valeurs "trouv�es" � t�tons)
+		Delay.msDelay(510);
+		stop();
+	}
+	
+	public void demiTourGauche() {
+		roueG.setPower(40);
+		roueD.setPower(40);
+		
+		roueG.forward();
+		roueD.backward();
+		
+		Delay.msDelay(600);
+		stop();
+	}
+	
+	public void quartTourDroite() {
+		roueG.setPower(40);
+		roueD.setPower(40);
+		
+		roueD.forward();
+		roueG.backward();
+		
+		Delay.msDelay(300);
+		stop();
+	}
+	
+	//arret des moteurs et liberation des ressources
+	public void stop() {
+		roueG.stop();
+		roueD.stop();
+		//roueG.close();
+		//roueD.close();
+	}
+	
+}
